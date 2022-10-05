3560c058009c146daea94ef235cb8f8719771441
Bryson Shelton <bryson1679@gmail.com>
2022-10-05 09:07:00 -0500

    adds new and create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def show
     @article = Article.find(params[:id])
   end
@@ -8,4 +7,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
